d1968631a85c4a7c34850f565a2a340daa752adb
milton-kabir <milton.cse.pust@gmail.com>
2021-08-12 06:08:23 +0600

    First Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,8 +1,24 @@
+//package search;
 package com.kabir.milton;
+
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        String[] ar = sc.nextLine().split(" ");
+        HashMap<String, Integer> myMap = new HashMap<>();
+        for (int i = 0; i < ar.length; i++) {
+            myMap.put(ar[i], i + 1);
+        }
+        String fnd = sc.nextLine();
+        if (myMap.containsKey(fnd)) {
+            System.out.println(myMap.get(fnd));
+        } else {
+            System.out.println("Not found");
+        }
     }
 }
