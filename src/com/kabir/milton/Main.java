e7b45f672856f4efd645c3dbfdcd98fd9188fc5b
milton-kabir <milton.cse.pust@gmail.com>
2021-08-12 08:22:35 +0600

    Third Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -21,35 +21,52 @@ public class Main {
                 myMap.computeIfAbsent(s.toLowerCase(Locale.ROOT), k -> new ArrayList<>()).add(st);
             }
         }
-        System.out.println();
-        System.out.println("Enter the number of search queries:");
-        cnt = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < cnt; i++) {
+        while (true) {
             System.out.println();
-            System.out.println("Enter data to search people:");
-            String st = sc.nextLine().toLowerCase(Locale.ROOT);
-            int ccc = 0;
-            Set<String> ss = new HashSet<>();
-            for (String ii : myMap.keySet()) {
-                if (ii.matches("(.*)" + st + "(.*)")) {
-                    ccc = 1;
-                    ArrayList<String> ans = (ArrayList<String>) myMap.get(ii);
-                    ss.addAll(ans);
-                }
-            }
-            if (ccc == 0) {
-                System.out.println("No matching people found.");
-            } else {
-                System.out.println("\nFound people:");
-                for (String s : lst) {
-
-                    if (ss.contains(s)) {
-                        System.out.println(s);
+            System.out.println("=== Menu ===\n" +
+                    "1. Find a person\n" +
+                    "2. Print all people\n" +
+                    "0. Exit");
+            int ck = Integer.parseInt(sc.nextLine());
+            if (ck == 1) {
+                System.out.println();
+                System.out.println("Enter a name or email to search all suitable people.");
+                String st = sc.nextLine().toLowerCase(Locale.ROOT);
+                int ccc = 0;
+                Set<String> ss = new HashSet<>();
+                for (String ii : myMap.keySet()) {
+                    if (ii.matches("(.*)" + st + "(.*)")) {
+                        ccc = 1;
+                        ArrayList<String> ans = (ArrayList<String>) myMap.get(ii);
+                        ss.addAll(ans);
                     }
                 }
+                if (ccc == 0) {
+                    System.out.println("No matching people found.");
+                } else {
+                    System.out.println("\nFound people:");
+                    for (String s : lst) {
+
+                        if (ss.contains(s)) {
+                            System.out.println(s);
+                        }
+                    }
+                }
+
+            } else if (ck == 2) {
+                System.out.println();
+                System.out.println("=== List of people ===");
+                for (String s : lst) {
+                    System.out.println(s);
+
+                }
+
+            } else if (ck == 0) {
+                System.out.println("Bye!");
+                break;
+            } else {
+                System.out.println("Incorrect option! Try again.");
             }
-
         }
-
     }
 }
